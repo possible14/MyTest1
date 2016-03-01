f2e4c8735f7f55797f2e4c2af8d9b5c016bbfe9e
yao <possible14@163.com>
2016-03-01 10:24:35 +0800

    test

--- app/src/main/java/com/example/yao/mytest/MainActivity.java
+++ app/src/main/java/com/example/yao/mytest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.yao.mytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
